df03601700485f2ea16f304302a5e78cc2350cfa
ninjen <ninjen@ninjenLab.ninjen>
2025-05-24 13:19:52 -0700

    actualizacion mayo 24

--- clientMulti.py
+++ clientMulti.py
@@ -2,6 +2,17 @@
 
 import socket 
 import threading
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
+
+def send_message(event, client_socket, username, text_widget, entry_widget):
+     message = entry_widget.get()
+     client_socket.sendall(f"{username} > {message}".encode())
+     
+     entry_widget.delete(0, END)
+     text_widget.configure(state='normal')
+     text_widget.insert(END, f"{username} > {message}\n")
+     text_widget.configure(state='disabled')
 
 def client_program():
     
@@ -13,6 +24,21 @@ def client_program():
      
      username = input(f"\n[+] Introduce tu usuario: ")
      client_socket.sendall(username.encode())
+     
+     window = Tk()
+     window.title("Chat")
+     
+     
+     text_widget = ScrolledText(window, state='disabled')
+     text_widget.pack(padx=5, pady=5)
+     
+     entry_widget = Entry(window)
+     entry_widget.bind("<Return>", lambda event: send_message(event, client_socket, username, text_widget, entry_widget))
+     entry_widget.pack(padx=5, pady=5, fill=BOTH, expand=1)
+     
+     
+     window.mainloop()
+     client_socket.close()
      
 if __name__ == '__main__':
     client_program()    
