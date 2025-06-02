29ba4e6343d4fab048eda4caa09b3446a6474c70
NinjeNz <urias.la@proton.me>
2025-06-01 17:03:31 -0700

    junio 01 update

--- port_scanner.py
+++ port_scanner.py
@@ -125,6 +125,7 @@ if __name__=='__main__':
 
 # Refactorizado utilizando threading
 
+"""
 import socket
 import argparse 
 import threading
@@ -169,6 +170,96 @@ def scan_ports(ports, target):
     for thread in threads:
         thread.join()
 
+def parse_ports(ports_str):
+    if '-' in ports_str:
+        start, end = map(int, ports_str.split('-'))
+        return range(start, end+1)
+    elif ',' in ports_str:
+        return map(int, ports_str.split(','))
+    else:
+        return (int(ports_str),)
+    
+def main():
+    
+    target, ports_str = get_arguments()
+    ports = parse_ports(ports_str)
+    scan_ports(ports, target) 
+    
+if __name__=='__main__':
+    main()
+"""
+
+# Usando ThreadPoolExecutor y Signal
+
+import socket
+import argparse
+import signal
+#import sys
+import os
+from concurrent.futures import ThreadPoolExecutor
+from termcolor import colored
+
+open_sockets = []
+
+def def_handler(sig, frame):
+    print(colored(f"\n[!] Saliendo del programa...", 'red'))
+    
+    for socket in open_sockets:
+        socket.close()
+        
+    #sys.exit(1) #En mi caso no funciono, investigar el porque....
+    os._exit(os.EX_OK)
+    
+signal.signal(signal.SIGINT, def_handler) # Ctrl+c
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description='Fast TCP Port Scanner')
+    parser.add_argument("-t", "--target", dest="target", required=True, help="Victim target to scan (Ex: -t 192.168.1.1)")
+    parser.add_argument("-p", "--port", dest="port", required=True, help="Port range to scan (Ex: -p 1-100)")
+    options = parser.parse_args()
+    
+    #if options.target is None or options.port is None:
+    #    parser.print_help()
+    #    sys.exit(1)
+        
+    return options.target, options.port
+        
+def create_socket():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(1)
+    
+    open_sockets.append(s)
+    return s
+
+def port_scanner(port, host):
+    s = create_socket()
+    
+    try:
+        s.connect((host, port))
+        s.sendall(b"HEAD / HTTP/1.1\r\n\r\n")
+        response = s.recv(1024)
+        #response = response.decode(errors='ignore').split('\n')[0]
+        response = response.decode(errors='ignore').split('\n')
+        
+        if response:
+            print(colored(f"\n[+] El puerto {port} esta abierto\n", 'green'))
+            
+            for line in response:
+                print(colored(f"{line}", 'yellow'))
+        else:
+            print(colored(f"\n[+] El puerto {port} esta abierto", 'green'))
+        
+    except (socket.timeout, ConnectionRefusedError):
+        pass
+    
+    finally:
+        s.close()
+
+def scan_ports(ports, target):
+    
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        executor.map(lambda port: port_scanner(port, target), ports)
+        
 def parse_ports(ports_str):
     if '-' in ports_str:
         start, end = map(int, ports_str.split('-'))
