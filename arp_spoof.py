29ca44ec3aec8e8fe6d99a478b35be7b47463a09
NinjeNz <urias.la@proton.me>
2025-06-22 22:37:08 -0700

    arp spoofer 2

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+
+import argparse
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description="ARP Scanner")
+    parser.add_argument("-t", "--target", required=True, dest="target", help="Host / IP Scan")
+    
+    args = parser.parse_args()
+    
+    return args.target
+
+def main():
+    arguments = get_arguments()
+    
+if __name__ == '__main__':
+    main()
+        
+        
